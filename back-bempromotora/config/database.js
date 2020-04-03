a425a5e2c9ec98c1c20fc5a13fcdb1c1488f6e72
jrodrig30 <62968097+jrodrig30@users.noreply.github.com>
2020-04-03 11:23:34 -0300

    Update database.js

--- back-bempromotora/config/database.js
+++ back-bempromotora/config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-module.exports = mongoose.connect('mongodb://localhost:27017/dbEstudo', {
+module.exports = mongoose.connect('mongodb://localhost:27017/db-bempromotora', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
